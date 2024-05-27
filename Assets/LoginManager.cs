d6bd6619d87b000d5314d0515050f71e069cada5
Tomas Hauser <142511066+Hauserman@users.noreply.github.com>
2024-05-27 09:06:02 -0300

    Terminado clase 1

--- Assets/LoginManager.cs
+++ Assets/LoginManager.cs
@@ -1,9 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LoginManager : MonoBehaviour
 {
+
+    public InputField InputPassword;
+    public string Password = "Manzana";
+
+    public void OnClick()
+    {
+        if (InputPassword.text == Password)
+        {
+            Debug.Log("Acces granted");
+        }
+        else
+        {
+            Debug.Log("Acces denied");
+        }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,4 +32,5 @@ public class LoginManager : MonoBehaviour
     {
         
     }
+
 }
